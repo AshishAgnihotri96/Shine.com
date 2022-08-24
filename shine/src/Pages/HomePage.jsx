bc8b8b1f30d927513258c3b965f864cf2d1be55e
swatibersurda <swatibersurda@gmail.com>
2022-08-24 20:43:50 +0530

    adding_login_page

---
+++ shine/src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from "@chakra-ui/react"
+import { Login } from './Login'
+
+export const HomePage = () => {
+
+  return (
+    <div>
+        
+        <Login/>
+    </div>
+  )
+}
---
+++ shine/src/Pages/MainRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom"
+import { HomePage } from './HomePage'
+
+export const MainRoutes = () => {
+  return (
+  <Routes>
+    <Route path="/" element={<HomePage/>}></Route>
+  </Routes>
+  )
+}
