e02cd39f87ccd3edc50046096ca7d2bfe9316a16
Saurabh Kumar Singh <singhsaurabh9451985554@gmail.com>
2022-08-25 09:14:04 +0530

    added assistance

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Assistance from './components/Assistance';
+
+function App() {
+  return (
+    <div className="App">
+      <Assistance/>
+    </div>
+  );
+}
+
+export default App;
