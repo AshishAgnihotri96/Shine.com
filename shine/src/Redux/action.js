d98a276a22ae388a9d8761a414d6ed38fdbbd0a5
swatibersurda <swatibersurda@gmail.com>
2022-08-26 13:38:09 +0530

    addeing login

--- shine/src/Redux/action.js
+++ shine/src/Redux/action.js
@@ -15,3 +15,31 @@ export const getTasks = () => (dispatch) => {
     });
 };
 
+import * as Types from "./actionTypes";
+import axios from "axios";
+
+
+// thunk functiion
+export const loginData=(payload)=>(dispatch)=>{
+    console.log(payload)
+    dispatch({type:Types.LOGIN_REQUEST})
+    return axios.post("https://reqres.in/api/login",payload).then((res)=>{
+       dispatch({type:Types.LOGIN_SUCCESS,payload:res.data.token})
+       return Types.LOGIN_SUCCESS
+    }).catch((err)=>{
+        dispatch({type:Types.LOGIN_FALIURE,payload:err})
+        return Types.LOGIN_FALIURE
+    })
+}
+// 
+export const loginOutData=()=>(dispatch)=>{
+   
+    dispatch({type:Types.LOGOUT_REQUEST})
+    dispatch({type:Types.LOGOUT_SUCESS})
+        return Types.LOGOUT_SUCESS
+}
+
+    
+    
+
+
